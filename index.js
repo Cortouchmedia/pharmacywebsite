265590ec948987a868c468e3cf225b0447adfe19
Obafemi James <129944838+Cortouchmedia@users.noreply.github.com>
2023-12-27 12:47:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'bx bx-plus') {
+            icon.className = 'bx bx-minus';
+        } else {
+            icon.className = 'bx bx-plus';
+        }
+    })
+})
+
+
+
+// show/hide nav menu
+const menu = document.querySelector(".nav_menu");
+const menubtn = document.querySelector("#open-menu-btn");
+const closebtn = document.querySelector("#close-menu-btn");
+
+menubtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closebtn.style.display = "inline-block";
+    menubtn.style.display = "none";
+})
+
+// close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closebtn.style.display = "none";
+    menubtn.style.display = "inline-block"; 
+}
+
+closebtn.addEventListener('click', closeNav)
+
+
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".img-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+    btns.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            sliderNav(i)
+        });
+    });
+
+
+
